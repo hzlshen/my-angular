b7af9c0a8e6ea8ed7b20dd1680be385c77e6f2ce
hzlshen <m15277968105@163.com>
2019-01-18 17:28:24 +0800

    feat():配置404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { Code404Component } from './code404/code404.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePageComponent
+  },
+  {
+    path: '**',
+    component: Code404Component
   }
 ];
 
