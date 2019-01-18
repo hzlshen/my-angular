9fdf632f51e11906ddbb80d28aad76ff6433ddef
hzlshen <m15277968105@163.com>
2019-01-18 18:01:46 +0800

    router login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { Code404Component } from './code404/code404.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: LoginPageComponent
+  },
+  {
+    path: 'home',
     component: HomePageComponent
   },
   // 404 放最下面
