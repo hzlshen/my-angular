d7c9d8f42a55707911f86bbb2c08ab98eff9ffa9
hzlshen <m15277968105@163.com>
2019-01-22 12:16:54 +0800

    faet():左侧菜单

--- src/app/home-page/side-bar/side-bar.component.ts
+++ src/app/home-page/side-bar/side-bar.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-bar.component.scss']
 })
 export class SideBarComponent implements OnInit {
-
+  openMap = {
+    sub1: true,
+    sub2: false,
+    sub3: false
+  };
   constructor() { }
 
   ngOnInit() {
   }
-
+  openHandler(value: string): void {
+    for (const key in this.openMap) {
+      if (key !== value) {
+        this.openMap[ key ] = false;
+      }
+    }
+  }
 }
